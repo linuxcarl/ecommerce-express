ac5e79d96733b0c11ef6bf6d8f217c4e2f5f5ed0
Carlos Ramirez <carlos@soluciones360.com.mx>
2020-04-16 00:49:01 -0500

    se agrego estilos y assets para el diseno de la pagina

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const productsRouter = require('./routes/products')
 //con pug
 app.set("views", path.join(__dirname, 'views'));
 app.set("view engine", "pug");
-
+app.use("/static", express.static(path.join(__dirname, "public")));
 
 app.get('/', (req, res, next) => {
     res.render('index', { hello: 'Hola', world: 'Mundo! POST COVID 19' });
